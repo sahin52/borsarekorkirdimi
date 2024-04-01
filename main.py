75e3229257e0caecc88b050981ac3454aa18b385
sahin52 <sahinkasap52@gmail.com>
2024-04-01 22:00:43 +0300

    deploy is working fine at the moment

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from website import create_app
 
 app = create_app()
 
-if __name__ == '__main__' or __name__ == 'main':
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
     
     
