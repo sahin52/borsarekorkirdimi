67261fb617b17382e47c2a06e5e3beb72e51fef7
sahin52 <sahinkasap52@gmail.com>
2024-04-07 03:21:56 +0300

    updates for a bug about record breaking in the past days not changing db

--- website/views.py
+++ website/views.py
@@ -13,10 +13,13 @@ def home():
         xu100 = yf.Ticker('XU100.IS')
         xu100history = xu100.history(period='1d')
         current_xu100 = xu100history['Close'].iloc[-1]
-
+        # Get the highest value of xu100history['Close']
+        highest_xu100_value = xu100history['Close'].max()
+        
         # Get today's highest xu100 value
         todays_high_xu100 = xu100history['High'].iloc[-1]
-
+        # Find the date of the highest xu100 value
+        date_of_highest_xu100 = xu100history[xu100history['Close'] == highest_xu100_value].index[0].strftime('%Y-%m-%d')
         # Get highest xu100 value ever
         
          # Read the all-time high from the JSON file
@@ -25,12 +28,17 @@ def home():
             all_time_high = data['allTimeHigh']
             date_of_all_time_high = data['dateOfAllTimeHigh']
         # If today's high is greater than the all-time high, update the JSON file
-        if todays_high_xu100 > all_time_high:
-            all_time_high = todays_high_xu100
-            date_of_all_time_high = str(date.today())
+        if highest_xu100_value > all_time_high:
+            all_time_high = highest_xu100_value
+            date_of_all_time_high = date_of_highest_xu100
             with open('db.json', 'w') as file:
                 json.dump({'allTimeHigh': all_time_high, 'dateOfAllTimeHigh': date_of_all_time_high}, file)
-        return current_xu100, todays_high_xu100, all_time_high, date_of_all_time_high
+        return {
+            'current_xu100': current_xu100,
+            'todays_high_xu100': todays_high_xu100,
+            'all_time_high': all_time_high,
+            'date_of_all_time_high': date_of_all_time_high
+        }
     
     return render_template("base.html",data = get_xu100_data())
 
@@ -44,7 +52,7 @@ def dolar_bazinda():
         currentUsdTry = yf.Ticker('USDTRY=X').history(period='1d')['Close'].iloc[-1]
         current_xu100_usd = current_xu100 / currentUsdTry
 
-         # Read the all-time high from the JSON file
+        # Read the all-time high from the JSON file
         with open('db.json', 'r') as file:
             data = json.load(file)
             all_time_high = data['allTimeHighUSD']
